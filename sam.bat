193bf23b3355234cc4758e4bcc6e14964f898add
hirprajapati051088 <hirprajapati0510@gmail.com>
2021-12-14 11:45:20 +0530

    My First Commit

---
+++ sam.bat
@@ -0,0 +1,3 @@
+cd C:\Users\IM0258\eclipse-workspace\matrix_web
+
+java -cp C:\Users\IM0258\eclipse-workspace\matrix_web\bin;C:\Users\IM0258\eclipse-workspace\matrix_web\lib\* org.testng.TestNG testng.xml
